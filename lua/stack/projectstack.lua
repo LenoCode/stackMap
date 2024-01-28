5746570bb6a0dc5909e289ab52d6f7d58add3333
leno <leno.application@gmail.com>
2024-01-28 12:35:03 +0100

    Button development

--- lua/stack/projectstack.lua
+++ lua/stack/projectstack.lua
@@ -67,7 +67,6 @@ vim.api.nvim_buf_set_lines(0, 0, -1, false, content)
         vim.api.nvim_buf_set_lines(bufnr, -1, -1, true, { button.text })
         vim.api.nvim_buf_set_option(bufnr, 'modifiable', false)
         vim.api.nvim_buf_set_keymap(bufnr,"n","<Cmd>lua <CR>",':lua '..button.action..'()<CR>',{noremap=true,silent=true})
-        vim.api.nvim_buf_set_keymap(bufnr, 'n', tostring(i), ':lua print("hello man")<CR>', {})
     end
 
 -- Set the content in the floating window
