8bee2acadebc7a4ce248493ff64305ae67993900
leno <leno.application@gmail.com>
2024-02-02 23:03:55 +0100

    Stack project

--- lua/stack/projectstack.lua
+++ lua/stack/projectstack.lua
@@ -24,7 +24,7 @@ end
 --GUI FUNCTIONS
 
  function getCurrentLine()
-  local current_line = nvim.api.get_current_line()
+  local current_line = vim.api.get_current_line()
   print(current_line)
 end
 
